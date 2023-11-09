f9de38f46ab518606395025ffc0bc1b66ca96a39
Harshal <harshal0921@gmail.com>
2023-11-09 12:18:53 +0530

    ex 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let arr = string.split("");
+  let revArr = arr.reverse();
+  let revString = revArr.join("");
+  return revString;
 };
 
 // Do not edit below this line
