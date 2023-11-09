791553787a91f87366b723a1e659f494c732b8ea
Harshal <harshal0921@gmail.com>
2023-11-09 11:58:01 +0530

    ex 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (inputString, times) {
+  if (times < 0) return "ERROR";
+  let finalString = "";
+  for (let i = 0; i < times; i++) {
+    finalString = finalString.concat(inputString);
+  }
+  return finalString;
 };
 
 // Do not edit below this line
