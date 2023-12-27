ccb54a871b85dfb63f326e8cbe78f044a1174b06
Harshal <harshal0921@gmail.com>
2023-12-28 00:09:12 +0530

    exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  let string = input
+    .toLowerCase()
+    .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "")
+    .split(" ")
+    .join("");
+  let updString = string.split("").reverse().join("");
+  if (updString === string) return true;
+  else return false;
 };
 
 // Do not edit below this line
