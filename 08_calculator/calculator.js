1f80f19d83d4e6c197e814d90b69a209a3d4243f
Harshal <harshal0921@gmail.com>
2023-12-27 23:35:18 +0530

    exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let difference = a - b;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let product = array[0];
+  for (let i = 1; i < array.length; i++) {
+    product = product * array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  let result = base;
+  for (i = 1; i < power; i++) {
+    result = result * base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0) {
+    return 1;
+  }
+  let factorial = 1;
+  for (i = a; i > 0; i--) {
+    factorial = factorial * i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +50,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
