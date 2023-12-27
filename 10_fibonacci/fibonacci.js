7924e0e7541195a106e80dd784efc0e964df48ce
Harshal <harshal0921@gmail.com>
2023-12-28 01:02:59 +0530

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
+  let series = [1, 1];
+  for (let i = 2; i <= num; i++) {
+    series[i] = series[i - 1] + series[i - 2];
+  }
+  return series[num - 1];
 };
 
 // Do not edit below this line
